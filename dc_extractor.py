aa4f4a972cb09f71a63dff5d72d6957092ee7480
rayphk <rayp0hk@gmail.com>
2024-11-15 00:31:20 +0800

    made the analysis adapt to other languages, fixed some bug

--- dc_extractor.py
+++ dc_extractor.py
@@ -1,6 +1,11 @@
 import discord
 import re
 from discord.ext import commands
+import csv
+from langdetect import detect, detect_langs
+from translate import Translator
+
+
 
 # Set up intents
 intents = discord.Intents.default()
@@ -12,37 +17,57 @@ intents.message_content = True  # Enable message content intent (required for re
 # Create a bot instance
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-# User IDs
-marco = 694840655448637490
-pan = 665883311553642516
-kwong = 265352522036805632
-
 # Regular expression to extract the sentiment score
 sentiment_pattern = re.compile(r'\(([-+]?\d*\.?\d+)\)')
+
+
+
+# initial state of bot
+count = 0 
 
 @bot.event
 async def on_ready():
     await bot.change_presence(status=discord.Status.online, activity=discord.Game("Analyzing Sentiments"))  # Set presence
     print(f'Logged in as {bot.user}')
 
+
 @bot.event
 async def on_message(message):
+    global count 
     if message.author == bot.user:
         return  # Ignore messages from the bot itself
     
-    count = 0
-    while ((message.author.id == marco or pan or kwong) or (count >= 1)):
+
+    if ((message.author.id != bot.user) and (count == 0)):
         await message.channel.send('早晨，我係你老豆 (i am online)')
         count += 1
 
     # Check if the message contains a sentiment score
     match = sentiment_pattern.search(message.content)
 
-    if match and (message.author.id == marco):
+    if match:
         sentiment_score = float(match.group(1))  # Extract the sentiment score as a float
-        text = message.content[:match.start()].strip()  # Get the message text
-        formatted_message = (text, sentiment_score)
+        user_text = message.content[:match.start()].strip()  # Get the message text
+        language = detect(user_text)
+
+        if language != 'en':
+            translator = Translator( from_lang = language, to_lang = "en")
+            translation = translator.translate(user_text)
+            formatted_message = (translation, sentiment_score)
+        else:
+            formatted_message = (user_text, sentiment_score)
         print(formatted_message)
+    
+
+
+        # Open the CSV file in append mode
+        with open('data.csv', 'a', newline='') as file:
+            writer = csv.writer(file)
+        # Write the new data
+            writer.writerow(formatted_message)
+
+            print("Data appended successfully.")
+
 
     # Check if the message content is "pan"
     if message.content.lower() == 'pan':
@@ -52,5 +77,5 @@ async def on_message(message):
     await bot.process_commands(message)
 
 # Run the bot with your token
-bot.run('MTMwNjI1ODA2NjUxMzE5OTE1NQ.GtbGJD.4rdzlZpUITbzaho6UN5rnTFvK1Z9HyatLyYPiA')
+bot.run('MTMwNjYyNzYwNTczODU1NzQ0MA.GgvPGU.AegXmhdOIUHY8uTvPiZUg73n9eZc7J2X0A5320')
 
